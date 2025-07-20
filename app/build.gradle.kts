448d1dddcafb1272b18ee3f2385f4d682d90f36d
emon <emoon.zeroos@gmail.com>
2025-07-20 19:04:00 +0600

    update dependency version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,12 @@ plugins {
 
 android {
     namespace = "com.emon.mycontactapp"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.emon.mycontactapp"
         minSdk = 23
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -36,6 +36,8 @@ android {
         debug {
             isDebuggable = true
             buildConfigField("String", "BASE_URL", "\"https://dev.gozayaan.com/\"")
+            enableAndroidTestCoverage = true
+            enableUnitTestCoverage = true
         }
         release {
             isMinifyEnabled = true
@@ -70,40 +72,36 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
     //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("com.google.dagger:hilt-android:2.57")
+    ksp("com.google.dagger:hilt-android-compiler:2.57")
 
     //retrofit
-    val retrofit = "2.9.0"
-    val okHttp = "4.12.0"
+    val retrofit = "3.0.0"
+    val okHttp = "5.1.0"
     implementation("com.squareup.retrofit2:retrofit:${retrofit}")
     implementation("com.squareup.retrofit2:converter-gson:${retrofit}")
     implementation("com.squareup.okhttp3:okhttp:${okHttp}")
     implementation("com.squareup.okhttp3:logging-interceptor:${okHttp}")
 
     //gson
-    val gson = "2.11.0"
+    val gson = "2.13.1"
     implementation("com.google.code.gson:gson:${gson}")
 
     //lifecycle
-    val lifecycleVersion = "2.9.0"
+    val lifecycleVersion = "2.9.2"
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
 
     // Navigation
-    val navVersion = "2.9.0"
+    val navVersion = "2.9.2"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
@@ -115,20 +113,40 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     //coil image loader
-    implementation("io.coil-kt:coil:2.3.0")
+    implementation("io.coil-kt:coil:2.7.0")
 
     //
-    implementation("com.intuit.sdp:sdp-android:1.1.0")
-    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
 
 
     //lottie
-    implementation ("com.airbnb.android:lottie:6.1.0")
+    implementation ("com.airbnb.android:lottie:6.6.7")
 
     //Firebase BoM
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
 
-    //Unit test
-    testImplementation("io.mockk:mockk:1.13.13")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    // Testing
+    testImplementation("io.mockk:mockk:1.14.5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Flow Testing
+    testImplementation("app.cash.turbine:turbine:1.2.1")
+
+    // RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    testImplementation("androidx.recyclerview:recyclerview:1.4.0")
+
+    // Android Testing
+    testImplementation("androidx.test:core:1.6.1")
+    testImplementation("androidx.test:runner:1.6.2")
+    testImplementation("androidx.test.ext:junit:1.2.1")
+    testImplementation("org.robolectric:robolectric:4.15.1")
+
+
 }
