faff7fc2ae60c9a6a735d89e36f5618e9d69e5a4
emon <emoon.zeroos@gmail.com>
2026-05-08 01:11:28 +0600

    added compose theme

---
+++ app/src/main/java/com/emon/mycontactapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.emon.mycontactapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650A4)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFF7D5260)
+
+// Custom colors
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val WhiteSmoke = Color(0xFFEFEFEF)
+val PantoneCoolGray = Color(0xFF8A8A8D)
+val Silver = Color(0xFFB9B9B9)
+val LightGray = Color(0xFFE5E5EA)
+val DarkGray = Color(0xFF4D4D4D)
+
